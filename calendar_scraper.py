5aa692ac5af32a4dbd8d4c23d2c63f4aa297c95c
Abishan <abiarul1016@gmail.com>
2026-04-09 16:55:27 -0400

    Updated workweek calculating logic, so that it calculates for current one, rather than the upcoming one (except on weekends)

--- calendar_scraper.py
+++ calendar_scraper.py
@@ -10,92 +10,95 @@ from googleapiclient.errors import HttpError
 # If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
 
-SKIP_CALENDAR_ID = '2cab442dcaa371859cc8c0137d96f06d48b4d2e85ee67d5ee8a505f18f74b357@group.calendar.google.com'
+SKIP_CALENDAR_ID = "2cab442dcaa371859cc8c0137d96f06d48b4d2e85ee67d5ee8a505f18f74b357@group.calendar.google.com"
+
 
 def main():
     upcoming_skips = get_upcoming_skips()
 
     # FOR TESTING REASONS
-    
+
     if upcoming_skips:
-      for skip in upcoming_skips:
-          print(f"{skip['name']}: {skip['date']} - {skip['weekday']}")
-          # print(skip['name'], skip['date'], skip['weekday'])
+        for skip in upcoming_skips:
+            print(f"{skip['name']}: {skip['date']} - {skip['weekday']}")
+            # print(skip['name'], skip['date'], skip['weekday'])
+
 
 def get_skip_calendar_id():
-  # Checks environment if calendar id is given, and if not checks through calendar for relevant calendar ID
-  ...
+    # Checks environment if calendar id is given, and if not checks through calendar for relevant calendar ID
+    ...
 
-def get_upcoming_skips(skip_calendar: str=SKIP_CALENDAR_ID):
-  
-  # Implement function to get SKIP Calendar ID based on Calendar name (SKIP) from Calendar list,
-  # if the id is not provided by user in environment—for now this is fine
-  skip_calendar = SKIP_CALENDAR_ID
 
-  """
+def get_upcoming_skips(skip_calendar: str = SKIP_CALENDAR_ID):
+
+    # Implement function to get SKIP Calendar ID based on Calendar name (SKIP) from Calendar list,
+    # if the id is not provided by user in environment—for now this is fine
+    skip_calendar = SKIP_CALENDAR_ID
+
+    """
   Gets all upcoming skips for the upcoming school week
   """
-  creds = None
-  # The file token.json stores the user's access and refresh tokens, and is
-  # created automatically when the authorization flow completes for the first
-  # time.
-  if os.path.exists("token.json"):
-    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-  # If there are no (valid) credentials available, let the user log in.
-  if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-      creds.refresh(Request())
-    else:
-      flow = InstalledAppFlow.from_client_secrets_file(
-          "credentials.json", SCOPES
-      )
-      creds = flow.run_local_server(port=0)
-    # Save the credentials for the next run
-    with open("token.json", "w") as token:
-      token.write(creds.to_json())
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
 
-  try:
-    service = build("calendar", "v3", credentials=creds)
+    try:
+        service = build("calendar", "v3", credentials=creds)
 
-    # Filter out events for the relevant work week
-    time_min, time_max = get_target_work_week()
+        # Filter out events for the relevant work week
+        time_min, time_max = get_target_work_week()
 
-    # Call the Calendar API
-    print("🚀 Getting the upcoming SKIP events for upcoming school week...\n")
+        # Call the Calendar API
+        print("🚀 Getting the upcoming SKIP events for upcoming school week...\n")
 
-    events_result = (
-        service.events()
-        .list(
-            calendarId=skip_calendar,
-            timeMin=time_min,
-            timeMax=time_max,
-            singleEvents=True,
-            orderBy="startTime",
+        events_result = (
+            service.events()
+            .list(
+                calendarId=skip_calendar,
+                timeMin=time_min,
+                timeMax=time_max,
+                singleEvents=True,
+                orderBy="startTime",
+            )
+            .execute()
         )
-        .execute()
-    )
-    events = events_result.get("items", [])
+        events = events_result.get("items", [])
 
-    if not events:
-      print("No upcoming SKIP events found.")
-      return
+        if not events:
+            print("No upcoming SKIP events found.")
+            return
 
-    # Cleans the data to put in dictionary with only relevant information
-    upcoming_skips = []
+        # Cleans the data to put in dictionary with only relevant information
+        upcoming_skips = []
 
-    for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        weekday = dt.datetime.fromisoformat(start).weekday()
+        for event in events:
+            start = event["start"].get("dateTime", event["start"].get("date"))
+            weekday = dt.datetime.fromisoformat(start).weekday()
 
-        upcoming_skips.append({'name': event['summary'], 'date': start, 'weekday': weekday})
+            upcoming_skips.append(
+                {"name": event["summary"], "date": start, "weekday": weekday}
+            )
 
-    return upcoming_skips
+        return upcoming_skips
 
-  except HttpError as error:
-    print(f"An error occurred: {error}")
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+
 
 def get_target_work_week() -> tuple:
-
     """
     Logic which finds the upcoming work week depending on whether today's date is part of
     the upcoming work week
@@ -107,24 +110,25 @@ def get_target_work_week() -> tuple:
     today = dt.datetime.now()
     weekday = today.weekday()
 
-    if weekday == 6:
-    #   If today is Sunday apply the program on this week's work week
-        start_date = today + dt.timedelta(days=1)
+    if weekday == 5 or weekday == 6:
+        #   If today is the weekend apply the program on upcoming work week
+        days_until_monday = 7 - weekday
+        start_date = today + dt.timedelta(days=days_until_monday)
+
+        # End date span is always the Friday of the same week
+        end_date = start_date + dt.timedelta(days=4)
     else:
-    #   If it's any other day of the week, use next week's work week events,
-    #   by calculating the days till next monday
-        
-        days_until_next_monday = 7 - weekday
-        start_date = today + dt.timedelta(days=days_until_next_monday)
+        #   If it's any other day of the week, start_date is the same day, until the end of the week
 
-    # End date span is always the Friday of the same week
-    end_date = start_date + dt.timedelta(days=4)
+        start_date = today
+        days_until_friday = 4 - weekday
+        end_date = today + dt.timedelta(days=days_until_friday)
 
-    time_min = start_date.replace(hour=0, minute=0, second=0).isoformat() + 'Z'
-    time_max = end_date.replace(hour=0, minute=0, second=0).isoformat() + 'Z'
+    time_min = start_date.replace(hour=0, minute=0, second=0).isoformat() + "Z"
+    time_max = end_date.replace(hour=23, minute=59, second=59).isoformat() + "Z"
 
     return time_min, time_max
-      
+
 
 if __name__ == "__main__":
-  main()
+    main()
--- main.py
+++ main.py
@@ -1,12 +1,45 @@
+import os
+
+from dotenv import load_dotenv
+from twilio.rest import Client
+
 from calendar_scraper import get_target_work_week, get_upcoming_skips
-from generate_phrase import human_format_date, file_format_date, generate_absent_list, generate_phrase
-from generate_audio import generate_audio
+from generate_audio import REFERENCE_AUDIO_TRANSCRIPT, generate_audio
+from generate_phrase import (
+    file_format_date,
+    generate_absent_list,
+    generate_phrase,
+    human_format_date,
+)
 
-SKIP_CALENDAR_ID = '2cab442dcaa371859cc8c0137d96f06d48b4d2e85ee67d5ee8a505f18f74b357@group.calendar.google.com'
+# GLOBAL VARIABLES
+SECRETS_FILE_PATH = "/Users/abishanarulselvan/CODING/snyder_pranker/secrets.env"
+load_dotenv(SECRETS_FILE_PATH)
 
-REFERENCE_AUDIO = 'reference_audios/appa_reference.wav'
-REFERENCE_AUDIO_TRANSCRIPT = 'reference_audios/appa_reference.txt'
-OUTPUT_FILEPATH = 'output_audios/'
+SKIP_CALENDAR_ID = "2cab442dcaa371859cc8c0137d96f06d48b4d2e85ee67d5ee8a505f18f74b357@group.calendar.google.com"
+
+# REF AUDIO VARIABLES
+REF_AUDIO_PATH = "reference_audios/appa_reference.wav"
+REF_AUDIO_TRANSCRIPT = "reference_audios/appa_reference.txt"
+
+# TWILIO VARIABLES
+ACCOUNT_SID: str | None = os.getenv("TWILIO_ACCOUNT_SID")
+AUTH_TOKEN: str | None = os.getenv("TWILIO_AUTH_TOKEN")
+
+MY_PHONE_NUMBER: str | None = os.getenv("MY_PHONE_NUMBER")
+TWILIO_PHONE_NUMBER: str | None = os.getenv("TWILIO_PHONE_NUMBER")
+
+# REQUIRED PHONE NUMBERS
+DAD_PHONE_NUMBER = os.getenv("DAD_PHONE_NUMBER")
+SCHOOL_PHONE_NUMBER = os.getenv("SCHOOL_PHONE_NUMBER")
+
+# SYSTEM PROMPT
+# SYSTEM_PROMPT_PATH = "/Users/abishanarulselvan/CODING/snyder_pranker/system_prompts/april_fools/april_fools_V2.txt"
+
+# FILE VARIABLES
+NGROK_URL = "https://incubous-caitlyn-herby.ngrok-free.dev"
+OUTPUT_FILE_DIRECTORY = "/Users/abishanarulselvan/CODING/Skipper/output_audios"
+
 
 def main():
 
@@ -24,30 +57,40 @@ def main():
     absent_list = generate_absent_list(upcoming_skips)
 
     if not absent_list:
-        print('No absences this week!')
+        print("No absences this week!")
         return
-    
-    print('PLANNED ABSENCES THIS WEEK:')
+
+    print("PLANNED ABSENCES THIS WEEK:")
     print(absent_list)
 
     print()
     generated_phrase = generate_phrase(start_date, end_date, absent_list)
 
-    print(f'\nCall Script: {generated_phrase}\n')
+    print(f"\nCall Script: {generated_phrase}\n")
 
     # 3. GENERATE AUDIO USING CLONED VOICE
     file_start_date = file_format_date(unformatted_start_date)
     file_end_date = file_format_date(unformatted_end_date)
 
-    output_filename = f'{OUTPUT_FILEPATH}{file_start_date}-{file_end_date}.wav'
+    output_filename = f"{OUTPUT_FILE_DIRECTORY}/{file_start_date}-{file_end_date}.wav"
 
     generate_audio(
-        reference_audio=REFERENCE_AUDIO,
+        reference_audio=REF_AUDIO_PATH,
         reference_audio_transcript=REFERENCE_AUDIO_TRANSCRIPT,
         output_file=output_filename,
-        text_to_generate=generated_phrase)
+        text_to_generate=generated_phrase,
+    )
 
     # 4. USE TWILIO TO PLACE THE CALL, DIAL ONE, AND PLAY THE AUDIO, AND HANG UP
+    client = Client(ACCOUNT_SID, AUTH_TOKEN)
+    client.calls.create(
+        url="https://incubous-caitlyn-herby.ngrok-free.dev/skipper.xml",
+        to=MY_PHONE_NUMBER,
+        # USE ONLY IN PRODUCTION
+        # to=SCHOOL_PHONE_NUMBER,
+        from_=DAD_PHONE_NUMBER,
+    )
+
 
 if __name__ == "__main__":
     main()
